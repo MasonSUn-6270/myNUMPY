e6cea813c6c93a870de0cd233bfe3e36164f4648
Mason Sun <980619554@qq.com>
2020-02-11 13:45:34 +0800

    learn style

---
+++ draw_plt/stylesshow.py
@@ -0,0 +1,13 @@
+from draw_plt import *
+x= np.linspace(-1,1,30)
+y=np.sin(x**2)
+z=np.random.randn(30)
+print(len(plt.style.available))
+for style in plt.style.available:
+    plt.style.use(style)
+    fig=plt.figure()
+    plt.bar(x,z,width=1/30)
+    plt.plot(x,y)
+    plt.title(style)
+    plt.savefig(f'./styles/f{style}')
+    print(f'{style} done!')
