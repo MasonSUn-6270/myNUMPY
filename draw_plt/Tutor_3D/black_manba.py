f8b39a930f16860980efb8de89ec9f70005a6eb3
Mason Sun <980619554@qq.com>
2020-02-20 17:12:57 +0800

    all style.png

--- draw_plt/Tutor_3D/black_manba.py
+++ draw_plt/Tutor_3D/black_manba.py
@@ -3,7 +3,7 @@ from draw_plt.Tutor_3D import *
 
 def c(): return np.random.randn(500)
 
-
+plt.style.use('dark_background')
 f = plt.figure()
 ax = Axes3D(f)
 ax.plot3D(c()*50, c()*30, np.ones_like(c())*(-190),':', ms=20,c='gold',alpha=0.3)
@@ -13,6 +13,7 @@ ax.plot3D(c()*20, c()*20, c()*20, '1', c='purple', ms=70)
 ax.plot3D(c()*10, c()*10, c()*10, '1', c='yellow', ms=120)
 ax.plot3D(c()*3, c()*3, c()*3, '1', c='purple', ms=50)
 ax.plot3D(c()*1, c()*1, c()*1, '1', c='white', ms=200)
+ax.set_title('Kobe',color='white')
 ax.text(0, 0, 10, 'R\nI\nP\n',color = 'white')
 for i in ax.get_xticklabels() + ax.get_yticklabels() + ax.get_zticklabels():
     i.set_color('none')
